79f6a74397bfe11dca4ec95b0d2a7542f6067309
shorifuddin2 <shorifuddin240915@gmail.com>
2022-04-30 03:15:59 +0600

    sarvire side link up addd

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 5000 || Port;
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000 ;
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER=vivoPhone}:${process.env.DB_PASS=rK8QudPUuXI1tSd3}@cluster0.cpiv5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
