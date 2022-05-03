f3cb2e56bbfe26d8ec8353eda47c5745fd3948c8
shorifuddin2 <shorifuddin240915@gmail.com>
2022-05-04 01:14:59 +0600

    servier api add

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000 ;
+require('dotenv').config()
 
+const app = express()
 
+//middleware
 app.use(cors())
 app.use(express.json())
 
 
-
-const uri = `mongodb+srv://${process.env.DB_USER=vivoPhone}:${process.env.DB_PASS=rK8QudPUuXI1tSd3}@cluster0.cpiv5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cpiv5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+  try {
+    await client.connect();
+    console.log('db connect')
 
+    const productCollection =client.db('vivoPhone').collection('Product');
+
+    app.get('/product', async (req, res)=>{
+
+   const query = {};
+    const cursor = productCollection.find(query);
+    const Product = await cursor.toArray();
+    res.send(Product)
+
+    })
+
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
@@ -21,5 +42,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log('Listening to port', port);
 })
